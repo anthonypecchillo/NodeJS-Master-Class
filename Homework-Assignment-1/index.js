9a0157f55893da34e9b04916f2ff5a57581dcc7b
anthonypecchillo <anthonypecchillo@gmail.com>
2018-08-08 16:47:42 -0400

    Add URL parsing

--- Homework-Assignment-1/index.js
+++ Homework-Assignment-1/index.js
@@ -4,14 +4,27 @@
 
 // Dependencies
 const http = require('http');
+const url = require('url');
 
 // The HTTP server should respond to all requests with a string
 const httpServer = http.createServer((req, res) => {
-  res.send('Hello World!\n');
+  
+  // Get the URL and parse it
+  const parsedURL = url.parse(req.url, true);
+
+  // Get the path
+  const path = parsedURL.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, '');  // Trims off the slashes from both sides
+                                                            // (Helps us to handle requests to /foo and /foo/ equally.)
+  
+  // Send the response
+  res.end(`Hello World!\n`);
+
+  // Log the request path
+  console.log(`Request was received on path: ${trimmedPath}`);
+
 });
 
 
 // Start the HTTP server and have it listen on port 3000
-httpServer.listen(3000, function() {
-  console.log('The HTTP server is listening on port 3000...');
-});
+httpServer.listen(3000, () => console.log(`The HTTP server is listening on port 3000...`));
